3c15734b1775b31ed1cadae98ab2587a405e3fea
natnaelasgedom <natnael.asgedom@gmail.com>
2020-07-08 13:34:27 +0200

    Added AddSamuraiWithRelatedData() and ClearTables()

--- EFSamurai.Data/EfMethods.cs
+++ EFSamurai.Data/EfMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using EFSamurai.Domain;
 
 namespace EFSamurai.Data
@@ -9,7 +10,11 @@ namespace EFSamurai.Data
     {
         public static int AddOneSamurai(string name)
         {
-            var samurai = new Samurai { Name = name };
+            Random random = new Random();
+            Array valuesHairStyle = Enum.GetValues(typeof(HairStyle));
+            HairStyle randomHairStyle = (HairStyle)valuesHairStyle.GetValue(random.Next(valuesHairStyle.Length));
+
+            var samurai = new Samurai { Name = name, HairStyle = randomHairStyle};
 
             using (var context = new SamuraiContext())
             {
@@ -22,10 +27,14 @@ namespace EFSamurai.Data
 
         public static ICollection<Samurai> AddSomeSamurais(ICollection<string> names)
         {
+            Random random = new Random();
+            Array valuesHairStyle = Enum.GetValues(typeof(HairStyle));
+            HairStyle randomHairStyle = (HairStyle)valuesHairStyle.GetValue(random.Next(valuesHairStyle.Length));
+
             ICollection<Samurai> addedSamurais = new List<Samurai>();
             foreach (var name in names)
             {
-                addedSamurais.Add(new Samurai {Name = name});
+                addedSamurais.Add(new Samurai {Name = name, HairStyle = randomHairStyle});
             }
 
             using (var context = new SamuraiContext())
@@ -187,6 +196,96 @@ namespace EFSamurai.Data
             using (var context = new SamuraiContext())
             {
                 context.Battles.AddRange(addedBattles);
+                context.SaveChanges();
+            }
+        }
+
+        public static int AddSamuraiWithRelatedData()
+        {
+            Random random = new Random();
+            Array valuesHairStyle = Enum.GetValues(typeof(HairStyle));
+            Array valuesQuoteStyle = Enum.GetValues(typeof(QuoteStyle));
+
+            using var context = new SamuraiContext();
+            var endSamurai = new Samurai
+            {
+                Name = "Monkey D. Luffy",
+                HairStyle = (HairStyle) valuesHairStyle.GetValue(random.Next(valuesHairStyle.Length)),
+                QuotesCollection = new List<Quote>()
+                {
+                    new Quote
+                    {
+                        QuoteStyle = (QuoteStyle) valuesQuoteStyle.GetValue(random.Next(valuesQuoteStyle.Length)),
+                        Text = "There is nothing outside yourself that can ever " +
+                               "enable you to get better, stronger, richer, quicker or smarter." +
+                               "Everything is within. Everything exists. Seek nothing outside of yourself."
+                    },
+                    new Quote
+                    {
+                        Text = "The perfect blossom is a rare thing. " +
+                               "You could spend your life looking for one, " +
+                               "and it would not be a wasted life.",
+                        QuoteStyle = (QuoteStyle) valuesQuoteStyle.GetValue(random.Next(valuesQuoteStyle.Length))
+                    }
+                },
+                SamuraiBattle = new List<SamuraiBattle>()
+                {
+                    new SamuraiBattle
+                    {
+                        Battle = new Battle
+                        {
+                            Name = "Battle of Sekigahara",
+                            Description = "The Battle of Sekigahara was a decisive battle on October 21, 1600 " +
+                                          "(Keichō 5, 15th day of the 9th month), that preceded the establishment of " +
+                                          "the Tokugawa shogunate.",
+                            IsBrutal = true,
+                            StartDate = new DateTime(1600,10,21),
+                            EndDate = new DateTime(1600,10,23),
+                            BattleLog = new BattleLog
+                            {
+                                Name = "BattleLog for battle of sekigahara",
+                                BattleEvent = new List<BattleEvent>()
+                                {
+                                    new BattleEvent
+                                    {
+                                        Description = "Description of battleEvent 1.1",
+                                        Summary = "Summary of battleEvent 1.1",
+                                        Order = 1
+                                    },
+                                    new BattleEvent
+                                    {
+                                        Description = "Description of battleEvent 1.2",
+                                        Summary = "Summary of battleEvent 1.2",
+                                        Order = 2
+                                    },
+                                    new BattleEvent
+                                    {
+                                        Description = "Description of battleEvent 1.3",
+                                        Summary = "Summary of battleEvent 1.3",
+                                        Order = 3
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                SecretIdentity = new SecretIdentity
+                {
+                    RealName = "Rurouni Kenshin"
+                }
+            };
+
+            context.Samurais.Add(endSamurai);
+            context.SaveChanges();
+            return endSamurai.ID;
+        }
+
+        public static void ClearTables()
+        {
+            using (var context = new SamuraiContext())
+            {
+                context.RemoveRange(context.Samurais);
+                context.RemoveRange(context.Battles);
                 context.SaveChanges();
             }
         }
