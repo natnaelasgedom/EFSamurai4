0974533edcccba6ac8567a44000c801784b26ab8
natnaelasgedom <natnael.asgedom@gmail.com>
2020-07-08 09:32:22 +0200

    Add project files.

---
+++ EFSamurai/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFSamurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
